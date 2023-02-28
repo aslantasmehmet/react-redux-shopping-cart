796a060ef74e4279fff51a426d630446fd62dbf7
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2023-02-28 16:37:30 +0300

    The product.js file that holds the product information has been created.

---
+++ src/api/product.js
@@ -0,0 +1,21 @@
+export const products = [
+    {
+      id: 1,
+      name: 'Pringles Acı & Baharat',
+      price: "₺55,99" ,
+      img_url : "https://cdn.getir.com/product/5825a0fbaec7580004ad60c0_tr_1644560078343.jpeg"
+    },
+    {
+        id: 2,
+        name: 'Ruffles Originals',
+        price: "₺32,99" ,
+        img_url : "https://cdn.getir.com/product/5f8380543eb6546a2775d008_tr_1657029934230.jpeg"
+    },
+    {
+        id: 3,
+        name: 'Eti Gong Pops Mısır Patlağı',
+        price: "₺17,60" ,
+        img_url : "https://getir.com/urun/eti-gong-pops-sade-JcblqpJP89/"
+    }
+  ];
+  
